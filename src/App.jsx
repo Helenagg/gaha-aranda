7ed5cc728bafd8a25db7407e2cb0f5c5607d3dbb
Helena <hgonzalezg18@gmail.com>
2024-12-03 18:15:11 +0100

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import './output.css'
-import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import './output.css';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
-
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

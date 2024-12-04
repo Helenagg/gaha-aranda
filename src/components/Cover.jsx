24925027d54a83b22953735adfada06e947527e2
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-12-04 13:07:25 +0100

    cover added

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,7 +1,13 @@
 const Cover = () => {
   return (
-    <div>Cover</div>
-  )
-}
+    <div className='relative h-screen overflow-hidden'>
+      <img
+        src='/assets/img/cover.jpg'
+        alt='Cover'
+        className='absolute top-0 left-0 w-full h-full object-contain md:object-cover'
+      />
+    </div>
+  );
+};
 
-export default Cover
+export default Cover;

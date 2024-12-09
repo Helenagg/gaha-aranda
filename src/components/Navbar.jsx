201335916d22090d8bf92b2b25b475034496d9f8
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-12-09 11:38:59 +0100

    accesibility button added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import AccessibleLink from './Accessibility/AccessibleLink';
+import AccessibleButton from './Accessibility/AccessibleButton';
 
 const Navbar = () => {
   const location = useLocation();
@@ -19,13 +20,14 @@ const Navbar = () => {
               alt='Logo Gaha Aranda'
             />
           </AccessibleLink>
-          <button
+          <AccessibleButton
             data-collapse-toggle='navbar-default'
             type='button'
             className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-primary rounded-lg md:hidden hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
             aria-controls='navbar-default'
-            aria-expanded='false'>
-            <span className='sr-only'>Abrir menu</span>
+            aria-expanded='false'
+            ariaLabel='Abrir menu'
+            >
             <svg
               className='w-5 h-5'
               aria-hidden='true'
@@ -40,7 +42,7 @@ const Navbar = () => {
                 d='M1 1h15M1 7h15M1 13h15'
               />
             </svg>
-          </button>
+          </AccessibleButton>
           <div className='hidden w-full md:block md:w-auto' id='navbar-default'>
             <ul className='flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
               <li>
